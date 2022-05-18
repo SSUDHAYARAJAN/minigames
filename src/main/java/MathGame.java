e80f34a52a87d96742368dfea76b8aac415e9aea
SSUDHAYARAJAN <102026028+SSUDHAYARAJAN@users.noreply.github.com>
2022-05-18 15:21:44 +0530

    some modifications in java MathGame.class

--- src/main/java/MathGame.java
+++ src/main/java/MathGame.java
@@ -6,16 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value  ="/Mathgame")
+@WebServlet(value  ="/getmathscore")
 public class MathGame extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        req.setAttribute("username","iu");
 //        RequestDispatcher rd = req.getRequestDispatcher("numbergame.jsp");
 //        rd.forward(req,res);
-        response.getWriter().println("<h5>Logged  as "+request.getParameter("username")+"</h5>");
-        request.setAttribute("username",request.getParameter("username"));
-        RequestDispatcher rd= request.getRequestDispatcher("numbergame.jsp");
-        rd.forward(request,response);
+//        response.getWriter().println("<h5>Logged  as "+request.getParameter("username")+"</h5>");
+//        request.setAttribute("username",request.getParameter("username"));
+//        RequestDispatcher rd= request.getRequestDispatcher("numbergame.jsp");
+//        rd.forward(request,response);
+        response.getWriter().println(request.getParameter("Mscore"));
     }
 }
